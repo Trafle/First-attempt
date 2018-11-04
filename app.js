8ef8ae85b320243ff11e82dc621ef3ef0d3bd01d
Trafle <igorokkarpiuk@gmail.com>
2018-11-04 20:32:25 +0200

    beautiful legs

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello!");
+let a = [
+      {name: 'Igor', app: 'google'},
+      {name: 'Barsik', app: 'Budka'},
+      {name: 'Kushka', app: 'Krugka'},
+      {name: 'Valya', app: 'Kalya'}
+];
+
+function code(n,p){
+      let s = a[n-1];
+      console.log(s[p]);
+}
+
+code(1, 'name');
+
+//prompt('which of 4?'), prompt("name || app ?")
